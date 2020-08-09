fcd09ac0d2ab945fe8408cfa10b54dfbf3b8e4e3
Ashish <Ashish@DESKTOP-HA5HP0N>
2020-08-09 14:15:30 +0530

    changes in employeerepo

--- src/main/java/com/learn/boot/jpa/EmployeeRepository.java
+++ src/main/java/com/learn/boot/jpa/EmployeeRepository.java
@@ -1,7 +1,13 @@
 package com.learn.boot.jpa;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 public interface EmployeeRepository extends JpaRepository<Employee, Integer> {
 
+	@Query("select e from Employee e")
+	List<Employee> getAllDetails();
+	
 }
