a80077cd100fade8113b9c128534cadfea0dcecd
Ashish <Ashish@DESKTOP-HA5HP0N>
2020-08-14 12:53:11 +0530

    JPA Enhancement(with collection and LOb)

--- src/main/java/com/learn/boot/jpa/Employee.java
+++ src/main/java/com/learn/boot/jpa/Employee.java
@@ -1,14 +1,30 @@
 package com.learn.boot.jpa;
 
+import java.util.Arrays;
+import java.util.Date;
+import java.util.List;
+
+import javax.persistence.CollectionTable;
 import javax.persistence.Column;
+import javax.persistence.ElementCollection;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.Lob;
+import javax.persistence.OrderColumn;
 import javax.persistence.Table;
+import javax.persistence.Temporal;
+import javax.persistence.TemporalType;
+
+import lombok.Data;
+import lombok.ToString;
 
 @Entity
 @Table(name="Employee")
+//@Data
+//@ToString
 public class Employee {
 
    @Id
@@ -21,35 +37,117 @@ public class Employee {
    private Double salary;
    @Column(name="deg")
    private String deg;
+   
+   @Temporal(TemporalType.DATE)
+   @Column(name="doj")
+   private Date doj;
+   
+   @Temporal(TemporalType.TIME)
+   @Column(name="toj")
+   private Date toj;
+   
+   @Temporal(TemporalType.TIMESTAMP)
+   @Column(name="dateTimeOfJoin")
+   private Date dateTimeOfJoin;
+
+  // @Lob
+   //private byte[] img;//to store image,audio,video,pdfs....
+   
+   //@Lob
+   //private char[] largetext;//for large text data
+   @ElementCollection
+   @CollectionTable(name="Dept",joinColumns =@JoinColumn(name="empid"))
+   @OrderColumn(name="deptindex")
+   @Column(name="dept")
+   private List<String> dept;
 
 public Integer getEid() {
 	return eid;
 }
+
 public void setEid(Integer eid) {
 	this.eid = eid;
 }
+
 public String getEname() {
 	return ename;
 }
+
 public void setEname(String ename) {
 	this.ename = ename;
 }
+
 public Double getSalary() {
 	return salary;
 }
+
 public void setSalary(Double salary) {
 	this.salary = salary;
 }
+
 public String getDeg() {
 	return deg;
 }
+
 public void setDeg(String deg) {
 	this.deg = deg;
 }
+
+public Date getDoj() {
+	return doj;
+}
+
+public void setDoj(Date doj) {
+	this.doj = doj;
+}
+
+public Date getToj() {
+	return toj;
+}
+
+public void setToj(Date toj) {
+	this.toj = toj;
+}
+
+public Date getDateTimeOfJoin() {
+	return dateTimeOfJoin;
+}
+
+public void setDateTimeOfJoin(Date dateTimeOfJoin) {
+	this.dateTimeOfJoin = dateTimeOfJoin;
+}
+
+public List<String> getDept() {
+	return dept;
+}
+
+public void setDept(List<String> dept) {
+	this.dept = dept;
+}
+
 @Override
 public String toString() {
-	return "Employee [eid=" + eid + ", ename=" + ename + ", salary=" + salary + ", deg=" + deg + "]";
+	return "Employee [eid=" + eid + ", ename=" + ename + ", salary=" + salary + ", deg=" + deg + ", doj=" + doj
+			+ ", toj=" + toj + ", dateTimeOfJoin=" + dateTimeOfJoin + ", dept=" + dept + "]";
 }
+
+
+/*public byte[] getImg() {
+	return img;
+}
+
+public void setImg(byte[] img) {
+	this.img = img;
+}
+
+public char[] getLargetext() {
+	return largetext;
+}
+
+public void setLargetext(char[] largetext) {
+	this.largetext = largetext;
+}*/
+
 
    
    
--- src/main/java/com/learn/boot/jpa/EmployeeRunner.java
+++ src/main/java/com/learn/boot/jpa/EmployeeRunner.java
@@ -1,5 +1,8 @@
 package com.learn.boot.jpa;
 
+import java.io.FileInputStream;
+import java.util.ArrayList;
+import java.util.Date;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -15,14 +18,69 @@ public class EmployeeRunner implements CommandLineRunner {
 	
 	@Override
 	public void run(String... args) throws Exception {
-		// TODO Auto-generated method stub
-       Employee employee=new Employee();
+		/** Find all method with Example class*/
+     /*  Employee employee=new Employee();
        employee.setDeg("SD");
        System.out.println("Testing");
        Example<Employee> example=Example.of(employee);
        List<Employee> employees= employeeRepository.findAll(example);
 		//employees.forEach(System.out::println);
-       System.out.println(employees);
+       System.out.println(employees);*/
+		
+		/**Save Data with Date,Time And TimeStamp*/
+		/*Employee employee=new Employee();
+		employee.setEname("CC");
+		employee.setDeg("Developer");
+		employee.setDateTimeOfJoin(new Date());
+		employee.setDoj(new Date());
+		employee.setSalary(60000.00);
+		employee.setToj(new Date());
+		employeeRepository.save(employee);*/
+		
+		
+		/** save date with Image and Large text*/
+	
+		/*FileInputStream fis=new 
+				FileInputStream("D:\\Spring Boot+Microservices(Naresh IT)\\image.png");
+		
+		byte[] img= new byte[fis.available()];
+		fis.read(img);
+		String text="hbcjbcjnkxbcsgcjabs jc bgcbc chcibh ihcibfhxjvgvcsichuisgcsbuicoiscpo "
+				+ "chubdccvtfcutv7tfyuguigdutgctvx v8fvuadusihojos7fv vin0hi0 b bkn"
+				+ "vjbdcicvyiufchfkv y8cgbjgfugudhklugcV yi0g-0 vyfACPO-[X Jhu gh o-o hc"
+				+ "nc 9v0n9hijon0kibcnjbcci yhvgk hjojiohcjnchu gugcu"
+				+ "jc ucgu hiig9un8yuhsaihixguvsijojf"
+				+ "jhci viv i jvithio9usoishIvhiuhgihp";
+		char[] text1=text.toCharArray();
+		
+		Employee employee=new Employee();
+		employee.setEname("CC");
+		employee.setDeg("Developer");
+		employee.setDateTimeOfJoin(new Date());
+		employee.setDoj(new Date());
+		employee.setSalary(60000.00);
+		employee.setToj(new Date());
+		employee.setImg(img);
+		employee.setLargetext(text1);
+		employeeRepository.save(employee);*/
+		
+		/** Save Data with Collection*/
+         
+		Employee employee=new Employee();
+		List<String> dept=new ArrayList<>();
+		dept.add("QA");
+		dept.add("DEV");
+		dept.add("UAT");
+		employee.setEname("CC");
+		employee.setDeg("Developer");
+		employee.setDateTimeOfJoin(new Date());
+		employee.setDoj(new Date());
+		employee.setSalary(60000.00);
+		employee.setToj(new Date());
+		employee.setDept(dept);
+		employeeRepository.save(employee);
+		
+		
 	}
 
 }
